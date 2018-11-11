406cde3dbc875b20bb347426f874e25101f6ae49
Olga <olga.myazina@outlook.com>
2018-11-11 16:35:05 +0300

    layout: EventPage

---
+++ src/common.blocks/EventsPage/EventsPage.tsx
@@ -0,0 +1,26 @@
+import * as React from 'react';
+
+import { Tile } from '../Tile';
+import { DeviceEvent } from '.';
+
+export default class EventsPage extends React.Component {
+  events: [] = [];
+  componentDidMount() {
+    fetch('data/events.json')
+      .then(res => res.json())
+      .then(data => {
+        this.events = data.events;
+        this.forceUpdate();
+      });
+  }
+
+  render() {
+    return (
+      <main className="EventsPage">
+        {this.events.map((event: DeviceEvent, index: number) => {
+          return <Tile key={index} event={event} size={event.size} />;
+        })}
+      </main>
+    );
+  }
+}
---
+++ src/common.blocks/EventsPage/index.ts
@@ -0,0 +1,38 @@
+import EventsPage from './EventsPage';
+import './EventsPage.css';
+export default EventsPage;
+
+export interface DeviceEvent {
+  type: string;
+  title: string;
+  source: string;
+  time: string;
+  description: null | string;
+  icon: string;
+  data: EventData | undefined;
+  size: EventSize;
+}
+
+export type EventData = EventDataAudio | EventDataGraph | EventDataImage | EventDataButtons;
+
+export type EventSize = 's' | 'm' | 'l';
+
+export interface EventDataAudio {
+  albumcover: string;
+  artist: string;
+  track: {
+    name: string;
+    length: string;
+  };
+  volume: number;
+}
+export interface EventDataGraph {
+  type: 'graph';
+  values: object[];
+}
+export interface EventDataImage {
+  image: string;
+}
+export interface EventDataButtons {
+  buttons: string[];
+}
