dc366d9518e2e9f0638285d988b0861289b85ec9
Olga <olga.myazina@outlook.com>
2018-11-11 11:58:54 +0300

    Компонент Menu

---
+++ src/common.blocks/Menu/Menu.tsx
@@ -0,0 +1,23 @@
+import * as React from 'react';
+import { Component } from 'react';
+import * as ReactDOM from 'react-dom';
+import { cn, ClassNameFormatter } from '@bem-react/classname';
+import Nav from '../Nav';
+
+interface IMenuProps {
+  isOpen: boolean;
+}
+
+export default class Menu extends Component<IMenuProps> {
+  cnMenu: ClassNameFormatter;
+  constructor(props: IMenuProps) {
+    super(props);
+    this.cnMenu = cn('Menu');
+  }
+  //todo: вынести данные
+  render() {
+    return (
+      <Nav list={['События', 'Сводка', 'Устройства', 'Сценарии']} class={this.cnMenu({ isOpen: this.props.isOpen })} />
+    );
+  }
+}
