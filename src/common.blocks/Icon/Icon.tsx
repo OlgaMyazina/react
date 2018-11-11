eecb7d2e6b200c7ed29c8e0d40b9ee5f9f3cef80
Olga <olga.myazina@outlook.com>
2018-11-11 16:35:24 +0300

    Компонент Icon

---
+++ src/common.blocks/Icon/Icon.tsx
@@ -0,0 +1,25 @@
+import * as React from 'react';
+import { Component } from 'react';
+import * as ReactDOM from 'react-dom';
+
+interface IClassName {
+  className: string;
+}
+interface IUrlName {
+  urlName: string;
+}
+
+export default class Icon extends Component<IUrlName & IClassName> {
+  icoNameUrl: string;
+  className: string;
+  constructor(props: any) {
+    super(props);
+    this.icoNameUrl = this.props.urlName;
+    this.className = this.props.className;
+  }
+  render() {
+    return (
+      <div className={`${this.className} Icon`} style={{ backgroundImage: `url(images/${this.icoNameUrl}.svg)` }} />
+    );
+  }
+}
