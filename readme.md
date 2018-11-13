59a25658c715d22a95600bbbe4eebb71a969a94a
Olga <olga.myazina@outlook.com>
2018-11-13 23:05:10 +0300

    Добавила файл readme

---
+++ readme.md
@@ -0,0 +1,14 @@
+#BEM
+
+device detector не настроен, переключение сборок c помощью import файле src/index.tsx
+Для build сборки запустить :
+
+```
+npm run build
+```
+
+Для dev сборки запустить:
+
+```
+npm run dev
+```
--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,12 @@
 import * as React from 'react';
-import { Component, Fragment } from 'react';
 import * as ReactDOM from 'react-dom';
 
-//import DeviceDetector from 'device-detector-js';
+//todo: Подключить юзер детектор
+//import DeviceDetector from 'device-detector-js/dist/index';
+
+//todo: переключение с помощью подключения import
 
 import { App } from './components/App/App@desktop';
 //import { App } from './components/App/App@mobile';
-//import Logo from './Logo';
-import { cn } from '@bem-react/classname';
-import { Registry, withRegistry } from '@bem-react/di';
-//import { Header } from './common.blocks/Header@desktop';
 
 ReactDOM.render(<App />, document.querySelector('.root'));
